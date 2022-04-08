2a17494b9f1a5b9cbbbd7c0736ba84361fe06ec6
Artem Mushtakov <a.vladimirovich@internet.ru>
2022-04-08 12:19:17 +0400

    Initial Commit

---
+++ MVC-Coordinator-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-Coordinator-Example
+//
+//  Created by Artem Mushtakov on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
